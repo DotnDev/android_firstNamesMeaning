dfc847cc7b3db022133db686d51b7ed97dff562e
Christelle Canovas <christelle.canovas@gmail.com>
2020-10-08 21:40:00 +0200

    Name info page done

--- app/src/main/java/canovas/firstnamesmeanings/FirstNameFragment.java
+++ app/src/main/java/canovas/firstnamesmeanings/FirstNameFragment.java
@@ -1,7 +1,6 @@
 package canovas.firstnamesmeanings;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,19 +12,28 @@ import androidx.fragment.app.Fragment;
 
 import com.google.gson.Gson;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import Models.Combination;
 import Models.FirstName;
 
 public class FirstNameFragment extends Fragment {
 
-    TextView name_txtView;
-    TextView gender_txtView;
-    TextView numbers_txtView;
-    TextView nameDay_txtView;
-    TextView origin_txtView;
-    TextView etymology_txtView;
-    TextView descriptionBe_txtView;
-    TextView descriptionLove_txtView;
-    TextView descriptionDo_txtView;
+    private TextView name_txtView;
+    private TextView gender_txtView;
+    private TextView numbers_txtView;
+    private TextView nameDay_txtView;
+    private TextView origin_txtView;
+    private TextView etymology_txtView;
+    private TextView title_descriptionBe_txtView;
+    private TextView title_descriptionLove_txtView;
+    private TextView title_descriptionDo_txtView;
+    private TextView descriptionBe_txtView;
+    private TextView descriptionLove_txtView;
+    private TextView descriptionDo_txtView;
 
     String firstNameData;
 
@@ -41,6 +49,9 @@ public class FirstNameFragment extends Fragment {
         nameDay_txtView = view.findViewById(R.id.name_nameDay_txtView);
         origin_txtView = view.findViewById(R.id.name_origin_txtView);
         etymology_txtView = view.findViewById(R.id.name_etymology_txtView);
+        title_descriptionBe_txtView = view.findViewById(R.id.name_desc_be_title_txtView);
+        title_descriptionLove_txtView = view.findViewById(R.id.name_desc_love_title_txtView);
+        title_descriptionDo_txtView = view.findViewById(R.id.name_desc_do_title_txtView);
         descriptionBe_txtView = view.findViewById(R.id.name_desc_be_txtView);
         descriptionLove_txtView = view.findViewById(R.id.name_desc_love_txtView);
         descriptionDo_txtView = view.findViewById(R.id.name_desc_do_txtView);
@@ -52,13 +63,19 @@ public class FirstNameFragment extends Fragment {
 
         //Get JSON data into FirstName Model thanks to Gson library
         Gson gson = new Gson();
-        FirstName firstName = gson.fromJson(firstNameData,FirstName.class);
+        final FirstName firstName = gson.fromJson(firstNameData,FirstName.class);
 
         String numbers = ""+firstName.getCombination().getNumber1() + firstName.getCombination().getNumber2() + firstName.getCombination().getNumber3();
-        String nameDay = "";
+        String nameDay = "?";
         if(firstName.getNameDay() != null){
             nameDay = ""+firstName.getNameDay().getDay()+firstName.getNameDay().getMonth();
         }
+
+        setTitles(firstName.getGender());
+
+        firstName.getCombination().setDescriptionBe(formatDescription(firstName.getCombination().getDescriptionBe(), firstName.getFirstName()));
+        firstName.getCombination().setDescriptionLove(formatDescription(firstName.getCombination().getDescriptionLove(), firstName.getFirstName()));
+        firstName.getCombination().setDescriptionDo(formatDescription(firstName.getCombination().getDescriptionDo(), firstName.getFirstName()));
 
         name_txtView.setText(firstName.getFirstName());
         gender_txtView.setText(firstName.getGender());
@@ -71,6 +88,25 @@ public class FirstNameFragment extends Fragment {
         descriptionDo_txtView.setText(firstName.getCombination().getDescriptionDo());
 
         return view;
+
+    }
+
+    private void setTitles(String gender) {
+
+        if(gender.equals("F")){
+            title_descriptionBe_txtView.setText(R.string.name_description_be_title_f);
+            title_descriptionLove_txtView.setText(R.string.name_description_love_title_f);
+            title_descriptionDo_txtView.setText(R.string.name_description_do_title_f);
+        }else{
+            title_descriptionBe_txtView.setText(R.string.name_description_be_title_m);
+            title_descriptionLove_txtView.setText(R.string.name_description_love_title_m);
+            title_descriptionDo_txtView.setText(R.string.name_description_do_title_m);
+        }
+    }
+
+    private String formatDescription(String description, String firstName){
+
+        return description.replace("<strong>%s</strong>", firstName);
 
     }
 
