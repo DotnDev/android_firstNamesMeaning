60bdb41b709531fb7c86560119ab4e83fbf1e6d4
Christelle Canovas <christelle.canovas@gmail.com>
2020-11-30 16:21:53 +0100

    removed unused code

--- app/src/main/java/canovas/firstnamesmeanings/VolleySingleton.java
+++ app/src/main/java/canovas/firstnamesmeanings/VolleySingleton.java
@@ -1,11 +1,7 @@
 package canovas.firstnamesmeanings;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.util.LruCache;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 /*
